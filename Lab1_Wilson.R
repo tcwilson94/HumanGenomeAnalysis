f895aaae7e65e06997f3c46a968e51a950ae37b4
Thomas Wilson <tcwilson@umass.edu>
2021-02-10 16:15:03 -0500

    commit

---
+++ Lab1_Wilson.R
@@ -0,0 +1,28 @@
+#Basic Computation
+#Simple example
+3 * 3 
+
+#Note order of operations
+3 + 3 / 3
+#Versus
+(3 +3) / 3
+
+#Assignment Statements
+x <- 3
+#Use RSTudio's keyboard shortcut Alt + -
+#Object names
+#R Data Types
+#Numeric - decimal values
+#Integer - invoke the as.integer function
+#Logical - compariosn between variables
+#Standard logical operations are &, | (or), ! (negation)
+#Character - represents string values in R
+#Complex - defined by the imaginery value i
+#Vectors - seuqence of data elements of the same basic type
+#Now you can do computations on a vector
+
+#EXERCISES
+#Exercise 1 (x=3, y=15)
+#Exercise 2 (x=2, y=3)
+#Exercise 3 (vector)
+#Exercise 4 (sort nucleotide vector)
